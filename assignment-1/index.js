3aacc492b1664233eb4c710bbf5c5945aacc608d
Deeptikandel <deeptikandel1@gmail.com>
2022-04-13 14:46:18 +0300

    Assignment 1 done

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -1,1 +1,16 @@
 // Your code here
+const start=document.querySelector(".start")
+const display = document.querySelector(".alertbtn")
+const countNum = () => {
+  console.log("Start loading....");
+  for (let i = 1; i < 1000000; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, 1000);
+
+  }
+  console.log("Loading....");
+}
+
+start.addEventListener("click", ()=>countNum());
+display.addEventListener("click", ()=>alert("hi"));
