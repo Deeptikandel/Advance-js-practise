b8a899f3f317430169a0d6fa98b41dd536b54f11
Deeptikandel <deeptikandel1@gmail.com>
2022-04-13 20:46:18 +0300

    changes made with clear time out

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -1,16 +1,23 @@
 // Your code here
-const start=document.querySelector(".start")
-const display = document.querySelector(".alertbtn")
+const start = document.querySelector(".start");
+const display = document.querySelector(".alertbtn");
 const countNum = () => {
-  console.log("Start loading....");
-  for (let i = 1; i < 1000000; i++) {
-    setTimeout(() => {
+  let num = 10e5;
+  let i = 1;
+  let myTimeout;
+  const startCount = () => {
+    if (i < num) {
       console.log(i);
-    }, 1000);
+      myTimeout = setTimeout(() => {
+        i++;
+        startCount();
+      }, 0);
+    } else {
+      clearTimeout(myTimeout);
+    }
+  };
+  startCount();
+};
 
-  }
-  console.log("Loading....");
-}
-
-start.addEventListener("click", ()=>countNum());
-display.addEventListener("click", ()=>alert("hi"));
+start.addEventListener("click", () => countNum());
+display.addEventListener("click", () => alert("hi"));
